0d8bb6b9fa1c0012bc559ed277002954a1f9c1b1
shahmir <shahmiralisyed@gmail.com>
2023-07-05 20:32:52 +1000

    added skills section with partial experiences

--- backend_sanity/schemas/index.ts
+++ backend_sanity/schemas/index.ts
@@ -4,5 +4,7 @@ import testimonials from './testimonials'
 import Abouts from './abouts'
 import Skills from './skills'
 import Projects from './projects'
+import Experiences from './experiences'
+import WorkExperiences from './workExperience'
 
-export const schemaTypes = [testimonials, Abouts, Skills, Projects]
+export const schemaTypes = [testimonials, Abouts, Skills, Projects, Experiences, WorkExperiences]
--- frontend-react/src/container/Skills/Skills.tsx
+++ frontend-react/src/container/Skills/Skills.tsx
@@ -1,15 +1,85 @@
 import React, {useState, useEffect} from 'react';
-import ReactToolTip from 'react-tooltip';
+import {Tooltip} from 'react-tooltip';
 import {motion} from 'framer-motion';
 import {client, urlFor } from '../../client';
 
 import { AppWrap } from '../../Wrapper';
 import './Skills.scss';
+import exp from 'constants';
+
+// interface Experiences{
+//   works: any[];
+// }
 
 const Skills = () => {
+    const [skills, setSkills] = useState([])
+    const [experiences, setExperiences] = useState([])
+
+    useEffect(() => {
+      const expQuery = '*[_type == "experiences"]';
+      const skillQuery = '*[_type == "skills"]';
+      client.fetch(expQuery).then((data) => 
+      { 
+        // console.log('before', data);
+        setExperiences(data);
+      })
+
+      client.fetch(skillQuery).then((data) => 
+      { 
+        setSkills(data);
+      })
+    }, [])
+
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='head-text'>Skills and Experience</h2>
+
+      <div className='app__skills-container'>
+        <motion.div
+        className='app__skills-list'
+        >
+          {skills?.map((skill:any)=> (
+            <motion.div
+              whileInView={{opacity:[0,1]}}
+              transition={{duration:.5}}
+              className='app__skills-item app__flex'
+              key={skill.name}
+            >
+              <div className='app__flex' style={{ background: skill.bgColor}}>
+                <img src={urlFor(skill.icon).url()} alt={skill.name}></img>
+              </div>
+              <p className='p-text'>{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+        <motion.div className='app__skills-exp'>
+          {/* {console.log('here', experiences)} */}
+          {experiences?.map((experience) => (
+            
+            // <>
+              /* <motion.div
+                whileInView={{opacity:[0,1]}}
+                transition={{duration:.5}}
+                className='app__skills-exp-work'
+                data-tip
+                data-for={work.name}
+                key={work.name}
+              >
+                <h4 className='bold-text'>{work.name}</h4>
+                <p className='p-text'>{work.company}</p>
+              </motion.div>
+              <Tooltip
+                id={work.name}
+                effect="solid"
+                arrowColor="#fff"
+                className="skills-tooltip"
+              ></Tooltip> */
+            // </>
+          ))}
+        </motion.div>
+      </div>
+    </>
   )
 }
 
-export default Skills
+export default AppWrap(Skills, 'skills');
