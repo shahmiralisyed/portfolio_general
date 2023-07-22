5d59b2e1ab01f3283f63476ea27d2d551596e8ab
shahmir <shahmiralisyed@gmail.com>
2023-07-22 21:47:48 +1000

    Changed the about section with grid layout

--- frontend-react/src/container/About/About.tsx
+++ frontend-react/src/container/About/About.tsx
@@ -24,12 +24,12 @@ const About = () => {
           abouts.map((about:any, index:number) => (
             <motion.div
             whileInView={{opacity:1}}
-            whileHover={{scale:1.1}}
+            // whileHover={{scale:1.1}}
             transition={{duration:0.5, type:'tween'}}
             className='app__profile-item'
             key = {about.title + index}
             >
-              <img src={urlFor(about.imgUrl).url()} alt={about.title}/>
+              {/* <img src={urlFor(about.imgUrl).url()} alt={about.title}/> */}
               <h2  style={{marginTop: 20}}>{about.title}</h2>
               <p  style={{marginTop: 10}}>{about.description}</p>
             </motion.div>
--- frontend-react/src/container/Skills/Skills.tsx
+++ frontend-react/src/container/Skills/Skills.tsx
@@ -96,5 +96,5 @@ const Skills = () => {
 export default AppWrap(
   MotionWrap(Skills, 'app__skills'), 
   'skills',
-  'app__whitebg'
+  'app__primaryaltbg'
 )
